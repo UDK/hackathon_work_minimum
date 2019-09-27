044c1382da2648ba9359e105303219b76d45fdd1
szfsfrsefgdg iskdmwhsd <udk1@bk.ru>
2019-09-28 00:57:25 +0300

    Протестируб без ассинзронности

--- hack_api/Controllers/ValuesController.cs
+++ hack_api/Controllers/ValuesController.cs
@@ -75,9 +75,9 @@ namespace hack_api.Controllers
             return "0";
         }
 
-        private static async Task Add(IMongoCollection<BsonDocument> collection, GetJSON note)
+        private static void Add(IMongoCollection<BsonDocument> collection, GetJSON note)
         {
-            await collection.InsertOneAsync(note.ToBsonDocument());
+             collection.InsertOneAsync(note.ToBsonDocument());
         }
         private static async Task<string> GetNotes(IMongoCollection<BsonDocument> collection)
         {
