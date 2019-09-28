725db93b4364104a35a07cb08515aa1022186515
szfsfrsefgdg iskdmwhsd <udk1@bk.ru>
2019-09-28 13:32:03 +0300

    ненужный данные выкинул(костыльно немного)

--- hack_api/Controllers/ValuesController.cs
+++ hack_api/Controllers/ValuesController.cs
@@ -9,6 +9,7 @@ using MongoDB.Driver;
 using Newtonsoft.Json;
 using hack_api.Model;
 using Microsoft.AspNetCore.Cors;
+using System.Runtime.CompilerServices;
 
 namespace hack_api.Controllers
 {
@@ -51,28 +52,48 @@ namespace hack_api.Controllers
         }
 
 
-        [HttpPost]
+        [HttpGet]
         [Route("all")]
-        public string PostPeople([FromBody] GetJSON jSON)
+        public string PostPeople()
         {
             //Вынести на уровень выше
             const string nameCollection = "Note";
             MongoClient client = new MongoClient("mongodb+srv://public:12345qwert@hackathon-giyck.mongodb.net/test?retryWrites=true&w=majority");
             var database = client.GetDatabase("coordinateHackathon");
             IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>(nameCollection);
-            switch (jSON.func)
-            {
-                case "ADD":
-                    Add(collection, jSON);
-                    return "1";
-                case "ALL":
-                    return GetNotes(collection).GetAwaiter().GetResult();
-                case "DEL":
-                    return Delete(collection, jSON.Id).GetAwaiter().GetResult().ToJson();
-                case "UPD":
-                    return Update(collection, jSON).GetAwaiter().GetResult();
-            }
-            return "0";
+            //switch (jSON.func)
+            //{
+            //    case "ADD":
+            //        Add(collection, jSON);
+            //        return "1";
+            //    case "ALL":
+            //        return GetNotes(collection).GetAwaiter().GetResult();
+            //    case "DEL":
+            //        return Delete(collection, jSON.Id).GetAwaiter().GetResult().ToJson();
+            //    case "UPD":
+            //        return Update(collection, jSON).GetAwaiter().GetResult();
+            //}
+            var dd = GetNotes(collection).GetAwaiter().GetResult();
+
+            return dd;
+            //List<Point> bsonElementsValid = new List<Point>(bsonElements.Count / 4);
+            //foreach (BsonDocument bsonValidTrue in bsonElements)
+            //{
+            //    if (bsonValidTrue.GetValue("valid").AsBoolean == true && bsonValidTrue.GetValue("X").AsDouble != 0)
+            //    {
+            //        bsonElementsValid.Add(new Point(bsonValidTrue.GetValue("X").AsDouble, bsonValidTrue.GetValue("Y").AsDouble));
+            //    }
+            //}
+            //MathIn checkPoints = new MathIn(bsonElementsValid);
+            //foreach (BsonDocument bsonValidTrue in bsonElements)
+            //{
+            //    if (bsonValidTrue.GetValue("valid").AsBoolean == true && bsonValidTrue.GetValue("X").AsDouble != 0)
+            //    {
+            //        bsonElementsValid.Add(new Point(bsonValidTrue.GetValue("X").AsDouble, bsonValidTrue.GetValue("Y").AsDouble));
+            //    }
+            //}
+            //checkPoints.pointInLine();
+            //return "1";
         }
 
         private static void Add(IMongoCollection<BsonDocument> collection, GetJSON note)
@@ -81,7 +102,7 @@ namespace hack_api.Controllers
         }
         private static async Task<string> GetNotes(IMongoCollection<BsonDocument> collection)
         {
-            var Items = await collection.Find(new BsonDocument()).ToListAsync();
+            var Items = await collection.Find(new BsonDocument()).Project("{_id:0,X:1,Y:1,level:1}").ToListAsync();
             return Items.ToJson();
         }
         private static async Task<string> Delete(IMongoCollection<BsonDocument> collection, string idRemove)
@@ -95,5 +116,44 @@ namespace hack_api.Controllers
             var result = await collection.ReplaceOneAsync(new BsonDocument("_id", new ObjectId(note.Id)),note.ToBsonDocument());
             return result.ToJson();
         }
+    }
+    class MathIn
+    {
+        List<Point> points;
+
+        public MathIn(List<Point> arPoints)
+        {
+            points = arPoints;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool pointInLine(Point pointCompare)
+        {
+            for(int twoPoint = 0; twoPoint<points.Count-1; twoPoint++)
+            {
+                double x1 = points[twoPoint].X;
+                double x2 = points[twoPoint+1].X;
+                double x3 = pointCompare.X;
+                double y1 = points[twoPoint].Y;
+                double y2 = points[twoPoint+1].Y;
+                double y3 = pointCompare.Y;
+                double A = y2 - y1;
+                double B = x1 - x2;
+                double C = x1 * (y1 - y2) + y1 * (x2 - x1);
+                double d = Math.Abs(A * pointCompare.X + B * pointCompare.Y + C) / Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2));
+            }
+            return false;
+        }
+    }
+
+    struct Point
+    {
+        public Point(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public double X;
+        public double Y;
     }
 }
--- hack_api/Model/GetJSON.cs
+++ hack_api/Model/GetJSON.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using hack_api.Model;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace workminimum.Models
 {
