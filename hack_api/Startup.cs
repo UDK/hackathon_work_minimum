833c1d5e9bc69b5c3c9d3a521d542790f7db4921
szfsfrsefgdg iskdmwhsd <udk1@bk.ru>
2019-09-28 15:29:04 +0300

    Настроил CORS

--- hack_api/Startup.cs
+++ hack_api/Startup.cs
@@ -25,11 +25,13 @@ namespace hack_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder => builder.AllowAnyOrigin());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
